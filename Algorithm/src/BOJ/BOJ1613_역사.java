b3142b6e61289956064cfe7a9f2d1cd49c9c67a1
hyunbyungrok <hyunbyungrok@gmail.com>
2020-07-28 17:10:32 +0900

    [수정] 시간 단축을 위한 코드수정 - boolean배열로 변경

--- Algorithm/src/BOJ/BOJ1613_역사.java
+++ Algorithm/src/BOJ/BOJ1613_역사.java
@@ -5,16 +5,17 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ1613_역사 {
-	static int n, k, dist[][];
+	static int n, k, s,e;
+	static boolean dist[][];
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken()); k = Integer.parseInt(st.nextToken());
-		dist = new int[n+1][n+1];
+		dist = new boolean[n+1][n+1];
 		for(int i=0; i<k; i++) {
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken()), e = Integer.parseInt(st.nextToken());
-			dist[s][e] = 1;
+			s = Integer.parseInt(st.nextToken()); e = Integer.parseInt(st.nextToken());
+			dist[s][e] = true;
 		}
 		
 		floydW();
@@ -22,25 +23,19 @@ public class BOJ1613_역사 {
 		k = Integer.parseInt(br.readLine());
 		for(int i=0; i<k; i++) {
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken()), e = Integer.parseInt(st.nextToken());
-			if(dist[s][e]>0) appendLine(sb, -1);
-			else if(dist[e][s]>0) appendLine(sb, 1);
-			else if(dist[s][e]==0) appendLine(sb, 0);
+			if(dist[s= Integer.parseInt(st.nextToken())][e= Integer.parseInt(st.nextToken())]) sb.append(-1).append("\n");
+			else if(dist[e][s]) sb.append(1).append("\n");
+			else sb.append(0).append("\n");
 		}
 		System.out.println(sb.toString());
 		
 	}
-	private static void appendLine(StringBuilder sb, int i) {
-		sb.append(i).append("\n");
-	}
 	private static void floydW() {
 		for(int k=1; k<=n; k++) {
 			for(int i=1; i<=n; i++) {
+				if(!dist[i][k]) continue;
 				for(int j=1; j<=n; j++) {
-					if(dist[i][k]==0 || dist[k][j]==0) continue;
-					int d = dist[i][k] + dist[k][j];
-					if(d==0) continue;
-					if(d>dist[i][j]) dist[i][j]=d;
+					if(dist[k][j]) dist[i][j]=true;
 				}
 			}
 		}
