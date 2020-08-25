fabbc22feee85a5e617b4598d2140b063c97edf0
hyunbyungrok <hyunbyungrok@gmail.com>
2020-08-25 20:42:33 +0900

    [풀이]BOJ1941_소문난칠공주
    bitmask를 사용해서 내가 지나갔던 값들에 대해서만 탐색을 진행한다.그렇게되면연결된 사람들끼리만의 탐색이 가능하고 그 bit값으로 방문체크를 해주면 준복된 조합의 탐색을 띄어넘을 수 있다.

---
+++ Algorithm/src/BOJ/BOJ1941_소무난칠공주.java
@@ -0,0 +1,64 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ1941_소무난칠공주 {
+	static final int SIZE = 5;
+	static final char 이다솜파 = 'S';
+	static char map[][];
+	static boolean visited[];
+	public static void main(String[] args) throws Exception{
+		input();
+		System.out.println(solve());
+	}
+	private static int solve() {
+		int rtv = 0;
+		for(int i=0; i<SIZE*SIZE; i++) {
+			int y = i/SIZE;
+			int x = i%SIZE;
+			if(map[y][x]==이다솜파) {
+				rtv+=dfs(1, 0, 1<<i, i);
+			}else {
+				rtv+=dfs(0, 1, 1<<i, i);
+			}
+		}
+		return rtv;
+	}
+	static int[] dy= {-1, 0, 1, 0}, dx= {0, 1, 0, -1};
+	private static int dfs(int dasom, int doyun, int bitV, int idx) {
+		if(doyun > 3) return 0;
+		if(dasom + doyun == 7) {
+			return 1;
+		}
+
+		int rtv = 0;
+		for(int i=0; i<=idx; i++) {
+			if((bitV&(1<<i)) == 0) continue;
+			int y = i/SIZE;
+			int x = i%SIZE;
+			for(int d=0; d<4; d++) {
+				int ny = y + dy[d];
+				int nx = x + dx[d];
+				int nextV = SIZE*ny + nx;
+				if(ny < 0 || nx < 0 || ny>=SIZE || nx>=SIZE || visited[bitV|(1<<nextV)]) continue;
+				visited[bitV|(1<<nextV)] = true;
+				if(map[ny][nx] == 이다솜파) {
+					rtv += dfs(dasom+1, doyun, bitV|(1<<nextV), nextV);
+				}else {
+					rtv += dfs(dasom, doyun+1, bitV|(1<<nextV), nextV);
+				}
+			}
+		}
+		return rtv;
+	}
+
+	private static void input() throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		map = new char[SIZE][SIZE];
+		for(int i=0; i<SIZE; i++) {
+			map[i] = br.readLine().toCharArray();
+		}
+		visited = new boolean[1<<(SIZE*SIZE)];
+	}
+}
